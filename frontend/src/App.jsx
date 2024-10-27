850802faadc4e7a0e57b471a7993ece29b1cfca2
Abhishek Kr <ak96166956@gmail.com>
2024-10-27 23:26:25 +0530

    Project initialised

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-rose-800 text-white'>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
